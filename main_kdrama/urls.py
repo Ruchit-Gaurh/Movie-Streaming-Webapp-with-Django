9b9ebb23026877ad7cb866dbe5d2eb1e0c053aef
raju-pawar <64689277+raju-pawar@users.noreply.github.com>
2021-05-30 22:12:42 +0530

    jdls

--- main_kdrama/urls.py
+++ main_kdrama/urls.py
@@ -1,8 +1,10 @@
+from re import template
 from django.contrib import sitemaps
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 from .views import main_page, series_detail_view, play_view, search_result_page, sitemap_func
 from .sitemaps import StaticViewSitemap, slugsitemap, episodes
+from django.views.generic.base import TemplateView
 
 
 sitemaps={
@@ -19,4 +21,5 @@ urlpatterns = [
     path("series/<slug:title>/episode<int:number>", play_view, name='seriewithepisode'),
     path("search/results", search_result_page, name='search'),
     path('sitemap_index.xml', sitemap,{'sitemaps':sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt", TemplateView.as_view(template_name="main/robots.txt", content_type="text/plain"))
 ]
