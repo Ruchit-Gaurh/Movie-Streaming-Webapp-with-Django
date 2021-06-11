dfddbbc84360955a0c24b2efada27ae259652726
raju-pawar <64689277+raju-pawar@users.noreply.github.com>
2021-06-11 09:20:21 +0530

    jlskjfdj

--- main_kdrama/views.py
+++ main_kdrama/views.py
@@ -33,7 +33,7 @@ def main_page(request):
     cat8_name = category8_name.objects.all()
     cat9_name = category9_name.objects.all()
     cat10_name = category10_name.objects.all()
-    recent = upload_serie.objects.all().order_by('-id')[:10]
+    recent = upload_serie.objects.all().order_by('-id')[:20]
     context = {"poster":poster, 'cat1':cat1, 'cat2':cat2, 'cat3': cat3, 'cat4':cat4, 'cat5':cat5, 'cat6':cat6, 'cat7':cat7, 'cat8':cat8, 'cat9':cat9, 'cat10':cat10,'cat1_name':cat1_name, 'cat2_name':cat2_name, 'cat3_name': cat3_name, 'cat4_name':cat4_name, 'cat5_name':cat5_name, 'cat6_name':cat6_name, 'cat7_name':cat7_name, 'cat8_name':cat8_name, 'cat9_name':cat9_name, 'cat10_name':cat10_name, 'recent':recent}
     return render(request, "main.html", context)
 
